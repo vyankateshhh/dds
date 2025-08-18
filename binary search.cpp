5f40c2af3e9d3522e43817b654cfa878b9facf3f
vyankateshhh <vyankateshhh@gmail.com>
2025-08-18 13:29:36 +0530

    Create binary search.cpp

---
+++ binary search.cpp
@@ -0,0 +1,32 @@
+#include<iostream>
+using namespace std;
+int main(){
+   int arr[]= {10,20,40,80,100};
+   int val = 80;
+   int index = -1;
+   int n= sizeof(arr)/sizeof(arr[0]);
+   int low=0;
+   int high = n-1;
+   while(low<=high){
+       int mid =(low + high)/2;
+       
+   if(arr[mid]== val){
+       index = mid;
+       break;
+   }
+   else if(arr[mid]< val){
+       low = mid+1;
+   }
+   else {
+       high = mid-1;
+   }
+   }
+   if(index == -1){
+       cout << "not found";
+   }
+   else{
+       cout << "found at index:"<<index;
+   }
+}
+       
+       
