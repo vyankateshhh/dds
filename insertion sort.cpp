8b2045e9e7d0b9fb11a428cb228913f2fce03208
vyankateshhh <vyankateshhh@gmail.com>
2025-08-25 13:07:05 +0530

    Create insertion sort.cpp

---
+++ insertion sort.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+using namespace std;
+
+void insertionSort(int arr[], int n) {
+    for (int i = 1; i < n; i++) {
+        int temp = arr[i];
+        int j = i - 1;
+
+        while (j >= 0 && arr[j] > temp) {
+            arr[j + 1] = arr[j];
+            j--;
+        }
+        arr[j + 1] = temp;
+    }
+}
+
+int main() {
+    int arr[] = {5, 9, 3, 10, 2, 6};
+    int n = sizeof(arr) / sizeof(arr[0]);
+
+    insertionSort(arr, n);
+
+    cout << "Sorted array: ";
+    for (int i = 0; i < n; i++) {
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+
+    return 0;
+}
